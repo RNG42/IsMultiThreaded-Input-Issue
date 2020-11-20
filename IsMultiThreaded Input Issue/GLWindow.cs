c560c493019579c205f9820d74b2713ac470b46b
RNG42 <21169392+RNG42@users.noreply.github.com>
2020-11-20 06:05:50 +0100

    Initial commit

---
+++ IsMultiThreaded Input Issue/GLWindow.cs
@@ -0,0 +1,37 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+
+
+namespace IsMultiThreaded_Input_Issue
+{
+    class GLWindow : GameWindow
+    {
+        public GLWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) 
+        {
+            VSync = VSyncMode.On;
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            if (MouseState.ScrollDelta.Y != 0)
+            {
+                Console.WriteLine("Mousewheel Y: " + MouseState.ScrollDelta.Y);
+            }
+
+            base.OnUpdateFrame(args);
+        }
+
+    }
+}
---
+++ IsMultiThreaded Input Issue/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenTK.Windowing.Desktop;
+
+namespace IsMultiThreaded_Input_Issue
+{
+    class Program
+    {
+        private static GameWindowSettings gameWindowSettings = new GameWindowSettings() { IsMultiThreaded = false, UpdateFrequency = 60 };
+        private static NativeWindowSettings nativeWindowSettings = new NativeWindowSettings() { };
+        private static GLWindow GLW;
+
+        static void Main(string[] args)
+        {
+            GLW = new GLWindow(gameWindowSettings, nativeWindowSettings);
+            GLW.Run();
+        }
+    }
+}
